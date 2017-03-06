9f8f553d73e02834f18fcb639c43b1e864ed1dcc
stefanie hansen <stefuhnee@gmail.com>
2017-03-06 14:52:33 -0800

    basic console app finished?

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!String.IsNullOrEmpty(Console.ReadLine()))
+            {
+                if (Console.ReadLine() == "start")
+                    Stopwatch.Start();
+                if (Console.ReadLine() == "stop")
+                    Console.WriteLine(Stopwatch.Stop());
+            }
+        }
+    }
+
+    class Stopwatch
+    {
+        private static DateTime StartTime;
+
+
+        public static void Start()
+        {
+            if (StartTime == null)
+                StartTime = DateTime.Now;
+           else
+                throw new InvalidOperationException();
+        }
+
+        public static TimeSpan Stop()
+        {
+            var timeLength = DateTime.Now - StartTime;
+            return timeLength;
+        }
+    }
+}
